9a368d7cff77ebfc7d9c75c740f01ce4e1b6f8b5
Dimsol <claudiosakaeshigemi@gmail.com>
2018-09-01 13:29:33 -0300

    33. Creating a Form to Search Recipes
    33. Creating a Form to Search Recipes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React , {Component} from 'react';
-import ReactDOM from 'react-dom';
+import SearchRecipes from './SearchRecipes';
+import '../styles/index.css';
+
 
 class App extends Component {
     render() {
         return (
             <div>
                 <h2> Recipe Finder.</h2>
+                <SearchRecipes/>
             </div>
         )
     }
 }
+
+
  export default App;
